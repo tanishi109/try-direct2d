169dbe6f3ba6d65470b50f77173539b2c773a66d
tanishi109 <note109@gmail.com>
2018-01-19 18:04:04 +0900

    add mathtools

--- try-direct2d/try-direct2d/Mathtool.cpp
+++ try-direct2d/try-direct2d/Mathtool.cpp
@@ -20,3 +20,13 @@ float Mathtool::radToDeg(float radian)
 {
     return radian * 180 / M_PI;
 }
+
+float Mathtool::getDistance(float x1, float y1, float x2, float y2)
+{
+    return hypot(x2 - x1, y2 - y1);
+}
+
+float Mathtool::getRadFromPos(float x1, float y1, float x2, float y2)
+{
+    return std::atan2(y2 - y1, x2 - x1);
+}
--- try-direct2d/try-direct2d/Mathtool.h
+++ try-direct2d/try-direct2d/Mathtool.h
@@ -6,5 +6,7 @@ public:
     ~Mathtool();
     static float degToRad(float degree);
     static float radToDeg(float radian);
+    static float getDistance(float x1, float y1, float x2, float y2);
+    static float getRadFromPos(float x1, float y1, float x2, float y2);
 };
 
