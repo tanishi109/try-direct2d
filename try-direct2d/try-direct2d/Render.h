d371d036f5dca1b08e0d81d10fb54b333477a179
tanishi109 <note109@gmail.com>
2018-01-21 17:55:45 +0900

    :shirt: private

--- try-direct2d/try-direct2d/Render.h
+++ try-direct2d/try-direct2d/Render.h
@@ -15,19 +15,21 @@ public:
     static void DrawCircle(int x, int y, int r, BrushType type);
     static void DrawText(int x, int y, int w, int h, std::wstring text);
     static void SetRotation(float degree, float centerX, float centerY);
-//private:
     static HRESULT CreateDeviceResources();
     static HRESULT CreateDeviceIndependentResources();
 
     static HWND m_hwnd;
+private:
+    static ID2D1SolidColorBrush* getBrush(BrushType type);
+
     static ID2D1Factory* m_direct2dFactory;
     static IDWriteFactory* m_dWriteFactory;
     static IDWriteTextFormat* m_textFormat;
     static ID2D1HwndRenderTarget* m_renderTarget;
-    static ID2D1SolidColorBrush* m_brush; // FIXME: ブラシやテキストのフォーマットあらかじめ用意しておかないといけないのはどうにかならない?
+    // FIXME: ブラシやテキストのフォーマットあらかじめ用意しておかないといけないのはどうにかならない?
+    static ID2D1SolidColorBrush* m_brush; 
     static ID2D1SolidColorBrush* m_brush_white;
     static ID2D1SolidColorBrush* m_brush_black;
     static ID2D1SolidColorBrush* m_brush_pink;
-    static ID2D1SolidColorBrush* getBrush(BrushType type);
 };
 
