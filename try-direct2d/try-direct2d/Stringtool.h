aac2ea9d75c3f0477728844a5a13448e893fd592
tanishi109 <note109@gmail.com>
2018-02-07 21:23:19 +0900

    :recycle: sprit source to out of class.

--- try-direct2d/try-direct2d/Stringtool.h
+++ try-direct2d/try-direct2d/Stringtool.h
@@ -9,25 +9,29 @@ public:
     ~Stringtool();
 
     template<typename Head, typename... Rest>
-    static std::string GetAsString(Head head, Rest... rest)
-    {
-        std::ostringstream oss;
-        oss << head;
-        std::string string = JoinAsString(oss.str(), rest...);
-
-        return string;
-    }
+    static std::string GetAsString(Head head, Rest... rest);
     static std::wstring ToWString(const std::string& refSrc);
 private:
     static std::string JoinAsString(std::string& string);
-
     template<typename Head, typename... Rest>
-    static std::string JoinAsString(std::string& string, Head head, Rest... rest)
-    {
-        std::ostringstream oss;
-        oss << string << head;
-
-        return JoinAsString(oss.str(), rest...);
-    }
+    static std::string JoinAsString(std::string& string, Head head, Rest... rest);
 };
 
+template<typename Head, typename... Rest>
+std::string Stringtool::GetAsString(Head head, Rest... rest)
+{
+    std::ostringstream oss;
+    oss << head;
+    std::string string = JoinAsString(oss.str(), rest...);
+
+    return string;
+}
+
+template<typename Head, typename... Rest>
+std::string Stringtool::JoinAsString(std::string& string, Head head, Rest... rest)
+{
+    std::ostringstream oss;
+    oss << string << head;
+
+    return JoinAsString(oss.str(), rest...);
+}
